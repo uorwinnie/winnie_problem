22f65ac1ba03e9cd0c103a66e31b8de6f5414e55
uorwinnie <wuthmonthant005@gmail.com>
2024-09-07 15:44:05 +0630

    Winnie commits in LYT branch

--- main.py
+++ main.py
@@ -21,16 +21,3 @@ if __name__ == "__main__":
     robot_name = Robot()
 
 """End here"""
-
-"""Winnie's solution in her main"""
-import random
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "1234567890"
-def robot_name():
-    letters = random.choices(alphabet,k=2)
-    digits = random.choices(numbers,k=3)
-    name = ''.join(letters + digits)
-    return name
-
-print(robot_name())
-"""ends here"""
