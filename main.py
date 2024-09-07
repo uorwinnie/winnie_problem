d4cdb2cbf27458d685d3c6bdd994ca43504964b0
uorwinnie <wuthmonthant005@gmail.com>
2024-09-07 15:10:02 +0630

    WHT commits in winnie_problem main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#lll
+"""Winnie's solution in her main"""
+import random
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+def robot_name():
+    letters = random.choices(alphabet,k=2)
+    digits = random.choices(numbers,k=3)
+    name = ''.join(letters + digits)
+    return name
+
+print(robot_name())
+"""ends here"""
